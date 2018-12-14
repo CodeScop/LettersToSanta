9724e11a0300ed3b651d02c5de1e26686ec7296c
bhanna1693 <bhanna1693@gmail.com>
2018-12-14 16:20:21 -0500

    add index page on controller

--- src/main/java/com/techtalentsouth/LettersToSanta/Letters/LettersController.java
+++ src/main/java/com/techtalentsouth/LettersToSanta/Letters/LettersController.java
@@ -1,5 +1,15 @@
 package com.techtalentsouth.LettersToSanta.Letters;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class LettersController {
 
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("blogpost/index");
+		return mv;
+	}
 }
