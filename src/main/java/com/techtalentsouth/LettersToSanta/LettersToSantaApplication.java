4c8e7d170ec1a0f05fd67b1c32a8ae437415eea2
Stefanie Ward <StefBWard@gmail.com>
2018-12-14 15:42:59 -0500

    initial commit

---
+++ src/main/java/com/techtalentsouth/LettersToSanta/LettersToSantaApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.LettersToSanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettersToSantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettersToSantaApplication.class, args);
+	}
+
+}
+
