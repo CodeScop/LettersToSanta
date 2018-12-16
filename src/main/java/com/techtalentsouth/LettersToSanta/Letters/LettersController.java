16b1aae998c8aa75f5eb2beb48af36d3ecbde6d7
bhanna1693 <bhanna1693@gmail.com>
2018-12-16 18:11:08 -0500

    add index page 'see all' functionality and created 'edit' and 'delete' functions as well

--- src/main/java/com/techtalentsouth/LettersToSanta/Letters/LettersController.java
+++ src/main/java/com/techtalentsouth/LettersToSanta/Letters/LettersController.java
@@ -1,10 +1,16 @@
 package com.techtalentsouth.LettersToSanta.Letters;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class LettersController {
@@ -14,8 +20,9 @@ public class LettersController {
 
 	//home page
 	@GetMapping("/")
-	public ModelAndView index() {
+	public ModelAndView index(Letters letters) {
 		ModelAndView mv = new ModelAndView("letters/index");
+		mv.addObject("letters", lettersRepository.findAll());
 		return mv;
 	}
 	
@@ -27,11 +34,36 @@ public class LettersController {
 	}
 	
 	//handles the saving of the letter and shows the letter after creation
-	@PostMapping("/letter/new_letter")
+	@PostMapping("/letters/new_letter")
 	public ModelAndView create_letter(Letters letters) {
 		ModelAndView mv = new ModelAndView("letters/letter-view");
 		Letters letter = lettersRepository.save(letters);
-		mv.addObject("letter", letters);
+		mv.addObject("letter", letter);
+		return mv;
+	}
+	
+	//shows the form for editing a letter
+	@GetMapping("/letters/edit/{id}")
+	public ModelAndView updateLetterForm(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("letters/edit");
+		Optional<Letters> post = lettersRepository.findById(id);
+		mv.addObject("letter", post);
+		return mv;
+	}
+	
+	//saves the edits to the letter
+	@PutMapping("/letters/edit")
+	public ModelAndView updateLetter(Letters letter) {
+		ModelAndView mv = new ModelAndView("redirect:/");
+		lettersRepository.save(letter);
+		return mv;
+	}
+	
+	//deletes letter
+	@DeleteMapping("/letters/delete/{id}")
+	public ModelAndView deleteLetter(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("redirect:/");
+		lettersRepository.deleteById(id);
 		return mv;
 	}
 }
